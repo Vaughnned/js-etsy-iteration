d04764e30b8a1b405a05a5471d1893d8607c0875
Vaughn Nedderman <vaughn@nedderman.com>
2023-05-09 22:19:35 -0400

    problems 2, 3, and 4 complete

--- scripts/main.js
+++ scripts/main.js
@@ -5,5 +5,25 @@
     const sum = usdItems.reduce((acc, item) => acc + item.price, 0);
     return sum / usdItems.length;
   }
-  console.log(usdAverage(items));
+  console.log("usdAverage", usdAverage(items));
+
+  function priceRange(arr, min, max) {
+    return arr.filter((item) => item.price >= min && item.price <= max);
+  }
+
+  console.log("priceRange", priceRange(items, 14, 18));
+
+  function findByCurrency(arr, code) {
+    return arr.find((item) => item.currency_code === code); // Using find() to find one specific currency code
+  }
+  let gbpItem = findByCurrency(items, "GBP");
+
+  console.log("findByCurrency", gbpItem.title, "£" + gbpItem.price);
+
+  function filterByMaterials(arr, material) {
+    const foo = arr.filter((item) => item.materials.includes(material)); // Filtering through to gather all items containing wood
+    return foo.map((item) => item.title); // Now map through the item to return just the title
+  }
+
+  console.log("filterByMaterials", filterByMaterials(items, "wood"));
 })();
