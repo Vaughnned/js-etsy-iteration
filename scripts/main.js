bc34c8585e67fcba4c38197a50040ba08fbcd34a
Vaughn Nedderman <vaughn@nedderman.com>
2023-05-09 21:42:15 -0400

    problem 1 complete

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,9 @@
 (function () {
   "use strict";
+  function usdAverage(arr) {
+    const usdItems = arr.filter((item) => item.currency_code === "USD");
+    const sum = usdItems.reduce((acc, item) => acc + item.price, 0);
+    return sum / usdItems.length;
+  }
+  console.log(usdAverage(items));
 })();
